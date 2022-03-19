fb3d8972ccc9d5020bbdc350239fa0ad222a33c4
NickCrespo <60528978+NickCrespo@users.noreply.github.com>
2022-03-19 16:13:37 -0700

    edits
    edit pause ui prefab(added options menu and full functionality)
    edit main menu scene(updated inspector)
    edit pause menu script(added functionality, game stops when paused, continues, restarts, and back to main menu
    edit settings script(added player prefs so when slider get changed in different scene changes carry over)
    adding scene templete settings?

--- WholeSome Game/Assets/Scripts/PauseMenu.cs
+++ WholeSome Game/Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenu;
+
+    public void Start()
     {
-        
+        Resume();
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Resume()
     {
-        
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        optionsMenu.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
--- WholeSome Game/Assets/Scripts/Settings.cs
+++ WholeSome Game/Assets/Scripts/Settings.cs
@@ -2,22 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Slider volSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    public void Start()
+    {
+        volSlider.value = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        musicSlider.value = PlayerPrefs.GetFloat("Music", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX", 1f);
+
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volSlider.value) * 20);
+        audioMixer.SetFloat("Music", Mathf.Log10(musicSlider.value) * 20);
+        audioMixer.SetFloat("SFX", Mathf.Log10(sfxSlider.value) * 20);
+
+    }
+
     public void SetTotalVolume(float volume)
     {
+        //volume = volSlider.value;
+        PlayerPrefs.SetFloat("MasterVolume", volume);
         audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+
+        //PlayerPrefs.SetFloat("MasterVolume", volume);
+
+        //PlayerPrefs.Save();
     }
 
     public void SetMusic(float volume)
     {
+        PlayerPrefs.SetFloat("Music", volume);
         audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
     }
 
     public void SetSFX(float volume)
     {
+        PlayerPrefs.SetFloat("SFX", volume);
         audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
     }
 }
